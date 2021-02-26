d6bb279cfd1fa0cff0aa1613a835db8cb5ab135c
Daniil <Popov>
2021-02-26 13:10:20 +0500

    Fix Spinner

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -9058,37 +9058,47 @@ module.exports = require("react-dropzone");
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+/* unused harmony export styles */
 /* unused harmony export Spinner */
-/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("Dtiu");
-/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("cDcd");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
-const Spinner = styled_components__WEBPACK_IMPORTED_MODULE_0___default.a.div`
-  display: inline-block;
-  margin: auto;
-  font-size: 10px;
-  position: relative;
-  text-indent: -9999em;
-  border: 0.2em solid rgba(0, 0, 0, 0.2);
-  border-left: 0.2em solid var(--default-color-accent);
-  transform: translateZ(0);
-  animation: load 450ms infinite linear;
+/*
+ * IMPORTANT: Don't use styled-components,
+ * because it will cause problems at the build stage
+ */
 
-  &,
-  &::after {
-    border-radius: 50%;
-    width: 3em;
-    height: 3em;
-  }
-
-  @keyframes load {
-    0% {
-      transform: rotate(0deg);
+const styles = {
+  display: 'inline-block',
+  margin: 'auto',
+  fontSize: '10px',
+  position: 'relative',
+  textIndent: '-9999em',
+  border: '0.2em solid rgba(0, 0, 0, 0.2)',
+  borderLeft: '0.2em solid var(--default-color-accent)',
+  borderRadius: '50%',
+  width: '3em',
+  height: '3em',
+  transform: 'translateZ(0)',
+  animation: 'load 450ms infinite linear',
+  '&::after': {
+    borderRadius: '50%',
+    width: '3em',
+    height: '3em'
+  },
+  '@keyframes load': {
+    '0%': {
+      transform: 'rotate(0deg)'
+    },
+    '100%': {
+      transform: 'rotate(360deg)'
     }
-    100% {
-      transform: rotate(360deg);
-    }
   }
-`;
+};
+const Spinner = () => __jsx("div", {
+  style: styles
+});
 /* harmony default export */ __webpack_exports__["a"] = (Spinner);
 
 /***/ }),
